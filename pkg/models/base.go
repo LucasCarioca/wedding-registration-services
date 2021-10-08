764abeba82b1c9744e30c50a7497620732125fa2
Lucas Desouza <lucasfds2777@gmail.com>
2021-10-08 00:41:39 -0500

    fix vet issue

--- pkg/models/base.go
+++ pkg/models/base.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Base struct {
-	ID        uint           `gorm:"primary_key" json:"id""`
+	ID        uint           `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
