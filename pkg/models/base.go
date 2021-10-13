3d13f8687b1a02285b8461f19a288f304eef83d7
Lucas Desouza <lucasfds2777@gmail.com>
2021-10-12 19:40:05 -0500

    update guest router security

--- pkg/models/base.go
+++ pkg/models/base.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"gorm.io/gorm"
 	"time"
 )
 
@@ -10,5 +9,4 @@ type Base struct {
 	ID        uint           `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
