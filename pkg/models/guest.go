996d1bcaa85bd8cba3433cd1f3a5888eba54d287
Lucas Desouza <lucas.desouza@aa.com>
2022-03-31 20:17:56 -0500

    clean up and keep dry

--- pkg/models/guest.go
+++ pkg/models/guest.go
@@ -3,10 +3,16 @@ package models
 //Guest model for the guest table
 type Guest struct {
 	Base
-	FirstName    string     `json:"first_name" binding:"required"`
-	LastName     string     `json:"last_name" binding:"required"`
-	Email        string     `json:"email" binding:"required"`
-	Phone        string     `json:"phone" binding:"required"`
-	InvitationID uint       `json:"invitation_id" binding:"required"`
-	Invitation   Invitation `json:"invitation" binding:"required"`
+	FirstName     string     `json:"first_name" binding:"required"`
+	LastName      string     `json:"last_name" binding:"required"`
+	Email         string     `json:"email" binding:"required"`
+	Phone         string     `json:"phone" binding:"required"`
+	EmailOptIn    bool       `json:"email_opt_in" binding:"required"`
+	SMSOptIn      bool       `json:"sms_opt_in" binding:"required"`
+	StreetAddress string     `json:"street_address" binding:"required"`
+	City          string     `json:"city" binding:"required"`
+	State         string     `json:"state" binding:"required"`
+	ZipCode       string     `json:"zip_code" binding:"required"`
+	InvitationID  uint       `json:"invitation_id" binding:"required"`
+	Invitation    Invitation `json:"invitation" binding:"required"`
 }
--- pkg/services/guest-service_test.go
+++ pkg/services/guest-service_test.go
@@ -1,10 +1,11 @@
 package services
 
 import (
+	"testing"
+
 	"github.com/LucasCarioca/wedding-registration-services/pkg/config"
 	"github.com/LucasCarioca/wedding-registration-services/pkg/datasource"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func Test_guest_services(t *testing.T) {
@@ -18,29 +19,33 @@ func Test_guest_services(t *testing.T) {
 			is.DeleteInvitationByID(int(i.ID))
 		}
 	}()
+	firstName := "tester"
+	lastName := "tester"
+	email := "tester@email.com"
+	phone := "5555555555"
+	emailOptIn := true
+	smsOptIn := true
+	street := "123 somewhere"
+	city := "some-city"
+	state := "some-state"
+	zip := "55555"
 	t.Run("should create a guest", func(t *testing.T) {
 		i := is.CreateInvitation("test", 1)
-		firstName := "tester"
-		lastName := "tester"
-		email := "tester@email.com"
-		phone := "5555555555"
-		g := gs.CreateGuest(firstName, lastName, email, phone, i)
+		g := gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i)
 		assert.NotNil(t, g.ID)
 		assert.Equalf(t, i.ID, g.InvitationID, "should have a foreign key to the invitation")
 		assert.Equalf(t, firstName, g.FirstName, "should have the right firstname")
 		assert.Equalf(t, lastName, g.LastName, "should have the right lastname")
 		assert.Equalf(t, email, g.Email, "should have the right email")
 		assert.Equalf(t, phone, g.Phone, "should have the right phone")
+		assert.Equalf(t, emailOptIn, g.EmailOptIn, "should have the right email opt in")
+		assert.Equalf(t, emailOptIn, g.SMSOptIn, "should have the right sms opt in")
 		gs.DeleteGuestByID(int(g.ID))
 	})
 
 	t.Run("should search for a guest by id", func(t *testing.T) {
 		i := is.CreateInvitation("test2", 1)
-		firstName := "tester"
-		lastName := "tester"
-		email := "tester@email.com"
-		phone := "5555555555"
-		id := gs.CreateGuest(firstName, lastName, email, phone, i).ID
+		id := gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i).ID
 		g, err := gs.GetGuestByID(int(id))
 		assert.Nil(t, err, "should not throw an error")
 		assert.Equalf(t, i.ID, g.InvitationID, "should have a foreign key to the invitation")
@@ -53,13 +58,9 @@ func Test_guest_services(t *testing.T) {
 
 	t.Run("should count the guests for an invitation", func(t *testing.T) {
 		i := is.CreateInvitation("test3", 3)
-		firstName := "tester"
-		lastName := "tester"
-		email := "tester@email.com"
-		phone := "5555555555"
-		g1 := gs.CreateGuest(firstName, lastName, email, phone, i)
-		g2 := gs.CreateGuest(firstName, lastName, email, phone, i)
-		g3 := gs.CreateGuest(firstName, lastName, email, phone, i)
+		g1 := gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i)
+		g2 := gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i)
+		g3 := gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i)
 		c := gs.GetGuestCountByInvitationID(i.ID)
 		assert.Equalf(t, 3, c, "should show the right guest count number")
 		gs.DeleteGuestByID(int(g1.ID))
@@ -69,11 +70,7 @@ func Test_guest_services(t *testing.T) {
 
 	t.Run("should be able to delete a guest by id", func(t *testing.T) {
 		i := is.CreateInvitation("test4", 1)
-		firstName := "tester"
-		lastName := "tester"
-		email := "tester@email.com"
-		phone := "5555555555"
-		id := gs.CreateGuest(firstName, lastName, email, phone, i).ID
+		id := gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i).ID
 		g, err := gs.GetGuestByID(int(id))
 		assert.Nil(t, err, "should not throw an error")
 		gs.DeleteGuestByID(int(g.ID))
@@ -84,13 +81,9 @@ func Test_guest_services(t *testing.T) {
 
 	t.Run("should get all guests for an invitation", func(t *testing.T) {
 		i := is.CreateInvitation("test5", 3)
-		firstName := "tester"
-		lastName := "tester"
-		email := "tester@email.com"
-		phone := "5555555555"
-		gs.CreateGuest(firstName, lastName, email, phone, i)
-		gs.CreateGuest(firstName, lastName, email, phone, i)
-		gs.CreateGuest(firstName, lastName, email, phone, i)
+		gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i)
+		gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i)
+		gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i)
 		guests, err := gs.GetAllGuestsByInvitationID(i.ID)
 		assert.Nil(t, err, "should not throw an error")
 		assert.Equalf(t, 3, len(guests), "should show the right number of guests in a list")
@@ -106,12 +99,8 @@ func Test_guest_services(t *testing.T) {
 	t.Run("should get all guests", func(t *testing.T) {
 		i1 := is.CreateInvitation("test6", 1)
 		i2 := is.CreateInvitation("test6", 1)
-		firstName := "tester"
-		lastName := "tester"
-		email := "tester@email.com"
-		phone := "5555555555"
-		gs.CreateGuest(firstName, lastName, email, phone, i1)
-		gs.CreateGuest(firstName, lastName, email, phone, i2)
+		gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i1)
+		gs.CreateGuest(firstName, lastName, email, phone, emailOptIn, smsOptIn, street, city, state, zip, i2)
 		guests := gs.GetAllGuests()
 		assert.Equalf(t, 2, len(guests), "should show the right number of guests in a list")
 		for _, g := range guests {
